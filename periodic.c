e79c40e3aaef44cdc1bd71b8f73085fb43fb8e54
Amcio <amcio122@gmail.com>
2023-01-01 21:49:25 +0100

    broken updateElement, memory shenanigans

--- periodic.c
+++ periodic.c
@@ -7,6 +7,22 @@
 #include <unistd.h> // ftruncate
 #include <ctype.h> // isspace()
 #define DBFile "elements.csv"
+typedef struct element {
+    char* name;
+    char* symbol;
+    uint8_t anum;
+    uint32_t amass;
+    char* comment;
+} element;
+
+int offsets[] = {
+    offsetof(element, name),
+    offsetof(element, symbol),
+    offsetof(element, anum),
+    offsetof(element, amass),
+    offsetof(element, comment)
+};
+
 /**
  * Remove trailing and leading whitespace from str
  * @param s Pointer to string
@@ -32,22 +48,86 @@ char* strstrip(char* s)
 
         return s;
 }
-
-typedef struct element {
-    char* name;
-    char* symbol;
-    uint8_t anum;
-    uint32_t amass;
-    char* comment;
-} element;
-
-int offsets[] = {
-    offsetof(element, name),
-    offsetof(element, symbol),
-    offsetof(element, anum),
-    offsetof(element, amass),
-    offsetof(element, comment)
-};
+/**
+ * Create a string for writing or reading into the csv file
+ * @param str char* for storing the string
+ * @param Element The element to convert
+ * @return 1 on any error
+*/
+int elementToStr(char** str, element* Element) {
+    if (0 > asprintf(str, "%s,%s,%d,%d,%s", Element->name, Element->symbol, Element->anum, Element->amass, Element->comment)) {
+        return 1;
+    }
+    return 0;
+}
+/**
+ * Search the list of elements based on one of the parameters
+ * @param query What to look for
+ * @param offset The offset to use for the field (0 or 1 for name or symbol respectively)
+ * @return Pointer to the element that matches the query, struct will contain NULL if failed
+*/
+element* searchElement(void* query, int offset) {
+    if (offset == 0 || offset == 1) {
+        // String
+    } else if (offset == 2 || offset == 3) {
+        // Int
+    } else {
+        // Error
+    }
+}
+/**
+ * Overwrite an element's data
+ * @param oldData The old element to look for
+ * @param newData THe new data to write
+ * @return Pointer to the newData struct, update the array with it
+*/
+element updateElement(element* oldData, element* newData) {
+    int count = 0;
+    char* line = NULL;
+    size_t len = 0; // Actual length that got read by getline()
+    ssize_t nread; // Size of data read by getline(), has to be signed
+    FILE *dbp, *tmpfile;
+    // here be demons
+    char* ElementStr, *ElementStrN, *ElementStrNew;
+    printf("ad");
+    asprintf(&ElementStr, "%s,%s,%d,%d,%s", oldData->name, oldData->symbol, oldData->anum, oldData->amass, oldData->comment);
+    // if (elementToStr(&ElementStr, oldData)) {
+    //     perror("Error creating ElementStr in updateElement()");
+    //     exit(1);
+    // }
+    printf("%s", ElementStr);
+    if (elementToStr(&ElementStrNew, newData)) {
+        perror("Error creating ElementStr in updateElement()");
+        exit(1);
+    }
+    strcpy(ElementStrN, ElementStr);
+    strcat(ElementStrN, "\n"); // Create a variation of our string with newline character at the end
+    dbp = fopen(DBFile, "r");
+    if (dbp == NULL) {
+        perror("Error opening DBFile in updateElement()");
+        exit(1);
+    }
+    tmpfile = fopen(".TEMPFILE", "w");
+    if (tmpfile == NULL) {
+        perror("Error opening temp file in updateElement()");
+        exit(1);
+    }
+    while ((nread = getline(&line, &len, dbp)) != -1) {
+        if ((strcmp(ElementStr, line) == 0)) {
+            fputs(ElementStrNew, tmpfile);
+        } else if (strcmp(ElementStrN, line) == 0) {
+            strcat(ElementStrNew, "\n");
+            fputs(ElementStrNew, tmpfile);
+        } else {
+            fputs(line, tmpfile);
+        }
+    }
+    fclose(tmpfile);
+    fclose(dbp);
+    remove(DBFile);
+    rename(".TEMPFILE", DBFile);
+    return *newData;
+}
 /**
  * Remove an element from the .csv file. This works be rewriting the whole file except for the line with our element
  * @param Element THe element to be removed
@@ -59,7 +139,7 @@ void removeElement(element* Element) {
     size_t read = 0;
     ssize_t nread;
     // ElementStr = How does the element look in csv format? The string WILL have a newline at the end
-    if (0 > asprintf(&ElementStr, "%s,%s,%d,%d,%s", Element->name, Element->symbol, Element->anum, Element->amass, Element->comment)) {
+    if (elementToStr(&ElementStr, Element)) {
         perror("Error creating ElementStr in removeElement()");
         exit(1);
     }
@@ -109,6 +189,7 @@ void saveElement(element* Element) {
  * @param length a variable to store the length of the array
  * @return An array filled with element structs
 */
+// TODO: INCOMPLETE, read only one digit for amass and anum, also size of elements array is hardcoded to 2
 element* readElements(size_t* length) {
     FILE* fp = fopen(DBFile, "r");
     if (fp == NULL) {
@@ -137,9 +218,9 @@ element* readElements(size_t* length) {
                 break;
             }
             case 1: {
-                char* n1 = malloc(strlen(token) + 1);
-                strcpy(n1, token);
-                Element.symbol = n1;
+                char* n = malloc(strlen(token) + 1);
+                strcpy(n, token);
+                Element.symbol = n;
                 break;
             }
             case 2:
@@ -149,9 +230,9 @@ element* readElements(size_t* length) {
                 *(uint32_t*)((char*)&Element+offsets[i]) = *token - '0';
                 break;
             case 4: {
-                char* n4 = malloc(strlen(token) + 1);
-                strcpy(n4, token);
-                Element.comment = n4;
+                char* n = malloc(strlen(token) + 1);
+                strcpy(n, token);
+                Element.comment = n;
                 break;
             }
             default:
@@ -184,6 +265,7 @@ int main() {
     printf("%s\n%d\n", elements[1].name, elements[1].anum);
     element test = {.name = "Nitrogen", .symbol = "N", .anum = 7, .amass = 14, .comment = "Favourite Element"};
     // saveElement(&test);
-    removeElement(&test);
+    // removeElement(&test);
+    updateElement(&(elements[1]), &test);
     return 0;
 }
